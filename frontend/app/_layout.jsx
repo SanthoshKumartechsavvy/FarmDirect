200696f27d3c5f60ed41a7c2287742d2d4ec4d6e
SanthoshKumartechsavvy <santhoshkumartechsavvy@gmail.com>
2024-08-21 00:54:55 +0530

    frontend: Sign-in page and verifyOtp completed

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -6,6 +6,10 @@ const TabLayout = () => {
   return (
     <Stack>
       <Stack.Screen name='index' options={{headerShown: false}}/>
+      <Stack.Screen
+            name="(auth)"
+            options={{ headerShown: false}}
+        />
     </Stack>
   )
 }
